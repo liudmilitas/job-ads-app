8411573515c29f82b44f95d3619fd7b647bb3481
Liudmila Kazakbaeva <liudmila.kazakbaeva@gmail.com>
2023-09-06 17:32:28 +0200

    add Job and Company as new custom object types

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,18 @@
+import { getJobs } from './db/jobs.js';
+import { getCompany } from './db/companies.js';
+
 export const resolvers = {
     Query: {
-        greeting: () => 'Hello World!'
-    }
+        jobs: () => getJobs(),
+    },
+
+    Job: {
+        date: (job) => toISODate(job.createdAt),
+        company: (job) => getCompany(job.companyId),
+    },
+    
 };
+
+function toISODate(date) {
+    return date.slice(0, 'yyyy-mm-dd'.length);
+}
