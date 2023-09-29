1362ca6c7b2504124303174d1724b05c7736739b
Liudmila Kazakbaeva <liudmila.kazakbaeva@gmail.com>
2023-09-29 23:43:46 +0200

    add custom error handler

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,11 +1,23 @@
 import { getJob, getJobs, getJobsByCompany } from './db/jobs.js';
 import { getCompany } from './db/companies.js';
+import { GraphQLError } from 'graphql';
 
 export const resolvers = {
     Query: {
-        job: (_root, { id }) => getJob(id),
+        job: async (_root, { id }) => {
+            const job = await getJob(id);
+            if (!job) {
+                throw notFoundError(`Job with id ${id} not found`);
+            }
+        },
         jobs: () => getJobs(),
-        company: (_root, { id }) => getCompany(id),
+        company: async (_root, { id }) => {
+            const company = await getCompany(id);
+            if (!company) {
+                throw notFoundError(`Company with id ${id} not found`);
+            }
+            return company;
+        },
 
     },
 
@@ -19,6 +31,12 @@ export const resolvers = {
     },
     
 };
+
+function notFoundError(error) {
+    return new GraphQLError(error, 
+        { extensions: { code: '404_NOT_FOUND' }}
+    );
+}
 
 function toISODate(date) {
     return date.slice(0, 'yyyy-mm-dd'.length);
