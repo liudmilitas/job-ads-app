a5fec4e3e00b2bbb4cf66e73bfff18b806d3c352
Liudmila Kazakbaeva <liudmila.kazakbaeva@gmail.com>
2023-10-12 00:42:11 +0200

    set fetchPolicy

--- server/scripts/create-db.js
+++ server/scripts/create-db.js
@@ -32,13 +32,13 @@ await schema.createTable('user', (table) => {
 await connection.table('company').insert([
   {
     id: 'FjcJCHJALA4i',
-    name: 'Facegle',
-    description: 'We are a startup on a mission to disrupt social search engines. Think Facebook meet Google.',
+    name: "Kiki's Delivery Service",
+    description: 'A local small business delivering your goods by broomstick',
   },
   {
     id: 'Gu7QW9LcnF5d',
-    name: 'Goobook',
-    description: 'We are a startup on a mission to disrupt search social media. Think Google meet Facebook.',
+    name: 'Le Blank',
+    description: 'We are a small aesthetic cafe serving aromatic curry and locally-grown coffee.',
   },
 ]);
 
@@ -46,23 +46,23 @@ await connection.table('job').insert([
   {
     id: 'f3YzmnBZpK0o',
     companyId: 'FjcJCHJALA4i',
-    title: 'Frontend Developer',
-    description: 'We are looking for a Frontend Developer familiar with React.',
-    createdAt: '2023-01-26T11:00:00.000Z',
+    title: 'Delivery Specialist',
+    description: 'We are looking for a delivery professional with extensive exprerience and a broom-flying lisence.',
+    createdAt: '2023-09-26T11:00:00.000Z',
   },
   {
     id: 'XYZNJMXFax6n',
     companyId: 'FjcJCHJALA4i',
-    title: 'Backend Developer',
-    description: 'We are looking for a Backend Developer familiar with Node.js and Express.',
-    createdAt: '2023-01-27T11:00:00.000Z',
+    title: 'Black Cat',
+    description: 'We are looking for an experienced black cat with exceptional communication skills.',
+    createdAt: '2023-09-27T11:00:00.000Z',
   },
   {
     id: '6mA05AZxvS1R',
     companyId: 'Gu7QW9LcnF5d',
-    title: 'Full-Stack Developer',
-    description: 'We are looking for a Full-Stack Developer familiar with Node.js, Express, and React.',
-    createdAt: '2023-01-30T11:00:00.000Z',
+    title: 'Cafe Assistant',
+    description: 'We are looking for a quick-witted and capable part-time staff cafe assitant.',
+    createdAt: '2023-10-10T11:00:00.000Z',
   },
 ]);
 
@@ -70,14 +70,14 @@ await connection.table('user').insert([
   {
     id: 'AcMJpL7b413Z',
     companyId: 'FjcJCHJALA4i',
-    email: 'alice@facegle.io',
-    password: 'alice123',
+    email: 'kiki@delivery.io',
+    password: 'kiki123',
   },
   {
     id: 'BvBNW636Z89L',
     companyId: 'Gu7QW9LcnF5d',
-    email: 'bob@goobook.co',
-    password: 'bob123',
+    email: 'boss@leblank.co',
+    password: 'boss123',
   },
 ]);
 
