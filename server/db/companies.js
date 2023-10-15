3eeebbccb44973effaa94844106f3b2002a5df94
Liudmila Kazakbaeva <liudmila.kazakbaeva@gmail.com>
2023-10-16 00:03:21 +0200

    implement per-request caching

--- server/db/companies.js
+++ server/db/companies.js
@@ -7,9 +7,9 @@ export async function getCompany(id) {
   return await getCompanyTable().first().where({ id });
 }
 
-export const companyLoader = new DataLoader( async (ids) => {
-  const companies = await getCompanyTable().select().whereIn('id', ids);
-
-  // ensuring the companies are rerurned in the same order as the ids
-  return ids.map(id => companies.find(company => company.id === id));
-});
+export function createCompanyLoader(){
+  return new DataLoader(async (ids) => {
+    const companies = await getCompanyTable().select().whereIn('id', ids);
+    return ids.map(id => companies.find(company => company.id === id));
+  });
+}
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,80 +1,94 @@
-import { createJob, getJob, getJobs, getJobsByCompany, deleteJob, updateJob } from './db/jobs.js';
-import { companyLoader, getCompany } from './db/companies.js';
-import { GraphQLError } from 'graphql';
+import {
+  createJob,
+  getJob,
+  getJobs,
+  getJobsByCompany,
+  deleteJob,
+  updateJob,
+} from "./db/jobs.js";
+import { getCompany } from "./db/companies.js";
+import { GraphQLError } from "graphql";
 
 export const resolvers = {
-    Query: {
-        job: async (_root, { id }) => {
-            const job = await getJob(id);
-            if (!job) {
-              throw notFoundError('No Job found with id ' + id);
-            }
-            return job;
-          },
-        jobs: () => getJobs(),
-        company: async (_root, { id }) => {
-            const company = await getCompany(id);
-            if (!company) {
-                throw notFoundError(`Company with id ${id} not found`);
-            }
-            return company;
-        },
-
+  Query: {
+    job: async (_root, { id }) => {
+      const job = await getJob(id);
+      if (!job) {
+        throw notFoundError("No Job found with id " + id);
+      }
+      return job;
     },
-
-    Job: {
-        date: (job) => toISODate(job.createdAt),
-        company: (job) => companyLoader.load(job.companyId),
+    jobs: () => getJobs(),
+    company: async (_root, { id }) => {
+      const company = await getCompany(id);
+      if (!company) {
+        throw notFoundError(`Company with id ${id} not found`);
+      }
+      return company;
     },
+  },
 
-    Company: {
-        jobs: (company) => getJobsByCompany(company.id),
+  Job: {
+    date: (job) => toISODate(job.createdAt),
+    company: (job, _args, { companyLoader }) => {
+      return companyLoader.load(job.companyId);
     },
+  },
 
-    Mutation: {
-        createJob: (_root, { input: { title, description } }, { user }) => {
-            if (!user) {
-                throw unauthorizedError('You must be signed in to create a job');
-            }
-            const companyId = 'FjcJCHJALA4i'; // hardcoded for now, change when we add auth
-            return createJob({ companyId, title, description });
-        },
-        deleteJob: async (_root, { id }, { user }) =>  {
-            if (!user) {
-                throw unauthorizedError('You must be signed in to delete a job');
-            }
-            const job =  await deleteJob(id, user.companyId);
-            if (!job) {
-                throw notFoundError(`No job found with id ${id}`);
-            }
-            return job;
-        },
-        updateJob: async (_root, { input: { id, title, description }}, { user }) => {
-            if (!user) {
-                throw unauthorizedError('You must be signed in to update a job');
-            }
-            const job = await updateJob({ id, companyId: user.companyId, title, description });
-            if (!job) {
-                throw notFoundError(`No job found with id ${id}`);
-            }
-            return job;
-        },
-    }
-    
+  Company: {
+    jobs: (company) => getJobsByCompany(company.id),
+  },
+
+  Mutation: {
+    createJob: (_root, { input: { title, description } }, { user }) => {
+      if (!user) {
+        throw unauthorizedError("You must be signed in to create a job");
+      }
+      const companyId = "FjcJCHJALA4i"; // hardcoded for now, change when we add auth
+      return createJob({ companyId, title, description });
+    },
+    deleteJob: async (_root, { id }, { user }) => {
+      if (!user) {
+        throw unauthorizedError("You must be signed in to delete a job");
+      }
+      const job = await deleteJob(id, user.companyId);
+      if (!job) {
+        throw notFoundError(`No job found with id ${id}`);
+      }
+      return job;
+    },
+    updateJob: async (
+      _root,
+      { input: { id, title, description } },
+      { user }
+    ) => {
+      if (!user) {
+        throw unauthorizedError("You must be signed in to update a job");
+      }
+      const job = await updateJob({
+        id,
+        companyId: user.companyId,
+        title,
+        description,
+      });
+      if (!job) {
+        throw notFoundError(`No job found with id ${id}`);
+      }
+      return job;
+    },
+  },
 };
 
 function notFoundError(error) {
-    return new GraphQLError(error, 
-        { extensions: { code: '404_NOT_FOUND' }}
-    );
+  return new GraphQLError(error, { extensions: { code: "404_NOT_FOUND" } });
 }
 
 function unauthorizedError(error) {
-    return new GraphQLError(error, 
-        { extensions: { code: 'NOT AUTHORIRED FOR THIS ACTION' }}
-    );
+  return new GraphQLError(error, {
+    extensions: { code: "NOT AUTHORIRED FOR THIS ACTION" },
+  });
 }
 
 function toISODate(date) {
-    return date.slice(0, 'yyyy-mm-dd'.length);
+  return date.slice(0, "yyyy-mm-dd".length);
 }
